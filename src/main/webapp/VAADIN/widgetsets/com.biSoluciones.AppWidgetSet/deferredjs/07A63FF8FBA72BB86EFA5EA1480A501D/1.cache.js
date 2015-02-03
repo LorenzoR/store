f0fb858640eb8b3467c6d01e4d535cd3a6fccce8
LorenzoR <lorenzorodrigo2000@hotmail.com>
2015-02-03 08:35:32 -0300

    initial commit

---
+++ src/main/webapp/VAADIN/widgetsets/com.biSoluciones.AppWidgetSet/deferredjs/07A63FF8FBA72BB86EFA5EA1480A501D/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_biSoluciones_AppWidgetSet.runAsyncCallback1('s8(461,1,hod);_.rc=function H$b(){_Pb((!WPb&&(WPb=new aQb),WPb),this.b.e)};pcd(Vh)(1);\n//# sourceURL=com.biSoluciones.AppWidgetSet-1.js\n')
